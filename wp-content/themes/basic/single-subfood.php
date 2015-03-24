59a21b8ff00c798485af8b41c725990ada2e0cfd
spaceboy360 <gregorykustanovich@gmail.com>
2015-03-23 20:39:28 -0700

    more changes

---
+++ wp-content/themes/basic/single-subfood.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+      
+      
+
+     <?php the_content(); ?>
+      <p>
+      	 
+
+           
+
+       
+       <div class="container setwidth">
+           
+                  
+                         <h1><?php the_title(); ?></h1>
+                              
+                  <div class="col-xs-6 ">
+                        <img class="img-responsive" src="<?php the_field('subthumbnail');?>" alt="">
+                  </div>
+
+        <div class="col-xs-6">
+            <?php the_field('subfoodtext'); ?>
+            <a href="<?php bloginfo('url');?>/home/" class="btn btn-primary btn-sm margin2">Go Back</a>
+        </div>
+
+      </div>
+
+
+
+      
+
+      <div class="col-xs-6">
+      	<img class="img-responsive" src="<?php the_field("image");?>" alt="">
+      </div>
+
+      </p>
+
+	<?php endwhile; else: ?>
+		<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
