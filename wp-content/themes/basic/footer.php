3a6ddb2f0d4832e614a452a1d0a41812834155ba
jon-gray <jon.gray@viscomcenter.com>
2015-03-24 21:43:47 -0700

    Styled elements
    Organized style.css code
    Changed search button to orange
    Changed nav bar style + added transition

--- wp-content/themes/basic/footer.php
+++ wp-content/themes/basic/footer.php
@@ -3,9 +3,11 @@
 	</div>
       
 	<div class="row white">
-		<div class="container footer setwidth">
+		<div class="container footer setwidth container-padding">
 			<div class="col-xs-12">
 				<h2 class="footertype1">The Chefs</h2>
+				<br>
+				<br>
 			</div>
 
 		
@@ -20,7 +22,7 @@
           
           <?php if ($chef->have_posts()) : while ($chef->have_posts()) : $chef->the_post(); ?>
 
-  <div class="col-xs-3 space5">    
+  <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 space5">    
       
    
 	<a class="box" data-toggle="modal" data-target="#m<?php the_id();?>" href="<?php the_field('link');?> " target="_blank">
