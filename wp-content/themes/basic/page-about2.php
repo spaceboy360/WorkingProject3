acc978c42d4db38c1767b7bc63b0df3bee520c61
spaceboy360 <gregorykustanovich@gmail.com>
2015-03-31 00:34:45 -0700

    about page update

--- wp-content/themes/basic/page-about2.php
+++ wp-content/themes/basic/page-about2.php
@@ -6,18 +6,19 @@ Template Name: About2 Page
 
 
 <div class="row gray2 down5">
-    <div class="container setwidth type1 container-padding-v">
-      <div class="col-xs-4"></div>
-      <div class="col-xs-4 centerabout">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nisi veritatis quas numquam nulla cum natus earum consequatur, tenetur molestias vero fugit dolorem officiis, voluptatum nemo commodi accusantium nesciunt? Mollitia.</p>
-      </div>
-      <div class="col-xs-4"></div>
-     
+    <div class="container setwidth type1 container-padding-v">  
 
+      <div class="col-xs-6">
+        <img class="img-responsive" src="http://images.clipartpanda.com/chef-hat-clipart-black-and-white-chef_says_okay_black_white_line_art-999px.png" alt="">
+      </div>
   
 
-  
+        <div class="col-xs-6">
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nisi veritatis quas numquam nulla cum natus earum consequatur, tenetur molestias vero fugit dolorem officiis, voluptatum nemo commodi accusantium nesciunt? Mollitia.</p>
+        </div>
+
       </div>
+
 </div>
 
 
