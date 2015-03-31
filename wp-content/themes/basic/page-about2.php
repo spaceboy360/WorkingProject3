a94986959aa90e20a7242144715c4d179f83bec8
spaceboy360 <gregorykustanovich@gmail.com>
2015-03-31 00:20:08 -0700

    made about page

---
+++ wp-content/themes/basic/page-about2.php
@@ -0,0 +1,29 @@
+<?php get_header();
+/*
+Template Name: About2 Page
+*/
+?>
+
+
+<div class="row gray2 down5">
+    <div class="container setwidth type1 container-padding-v">
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4 centerabout">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nisi veritatis quas numquam nulla cum natus earum consequatur, tenetur molestias vero fugit dolorem officiis, voluptatum nemo commodi accusantium nesciunt? Mollitia.</p>
+      </div>
+      <div class="col-xs-4"></div>
+     
+
+  
+
+  
+      </div>
+</div>
+
+
+ 
+
+
+
+<?php get_footer(); ?>
+
