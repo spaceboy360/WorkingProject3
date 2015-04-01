7a9f10a2faaccb0100b670b24e0b359d4d5e4188
spaceboy360 <gregorykustanovich@gmail.com>
2015-03-31 22:05:51 -0700

    Revert "fixed header nav"
    This reverts commit 2d37eb3e1b93678df02c9eed9d60658dcd30ecac.

--- wp-content/themes/basic/header.php
+++ wp-content/themes/basic/header.php
@@ -20,7 +20,7 @@
   <div class="container fullwidth">
   	
 	<div class="row gray2">
-	  	<div class="container fullwidth">
+	  	<div class="container">
 		  	<div class="navigation noborder spacing1" id="nav-padding">
 		  		<div class="col-xs-4 logodown">
 
